dfb8f02c31a0b236f6652402e0438a33e32c4179
madPhysix <froggyrain123@gmail.com>
2023-02-27 14:11:12 +0400

    Unsaved files repair

---
+++ Task7/task7.js
@@ -0,0 +1,22 @@
+let a='5'
+let num1=12
+let num2=10
+switch(a)
+{
+    case '+':
+        console.log(num1 + num2);
+        break;
+    case '-':
+        console.log(num1 - num2);
+        break;
+    case '*':
+        console.log(num1 * num2);
+        break;
+    case '/':
+        console.log(num1 / num2);
+        break;
+    default:
+        console.log("Invalid Input");
+        break;
+    
+}
